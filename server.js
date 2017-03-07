cdebfe5b1c63432466ebcc7f17608e8fd5c5ae23
Brandon <booth.brandon4@gmail.com>
2017-03-07 22:11:01 +0000

    set up the chatty server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
+    publicPath: config.output.publicPath, 
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000
